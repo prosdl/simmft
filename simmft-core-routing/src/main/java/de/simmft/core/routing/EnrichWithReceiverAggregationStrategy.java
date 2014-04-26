746bd8df345f445277b71cb1779ca8fc2c36c883
prosdl <rosendahl@prosdl.de>
2014-04-26 21:19:27 +0200

    Uses MftPath now.

--- simmft-core-routing/src/main/java/de/simmft/core/routing/EnrichWithReceiverAggregationStrategy.java
+++ simmft-core-routing/src/main/java/de/simmft/core/routing/EnrichWithReceiverAggregationStrategy.java
@@ -1,14 +1,14 @@
 package de.simmft.core.routing;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import de.simmft.common.path.MftPath;
+import de.simmft.common.path.MftPathException;
 
 @Component
 public class EnrichWithReceiverAggregationStrategy implements
@@ -23,23 +23,20 @@ public class EnrichWithReceiverAggregationStrategy implements
    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
       String pathToJob = (String) oldExchange.getIn().getBody();
 
-      // FIXME -> commons
-      // mft/outbox/{jobid}
-      String regex = ".*/outbox/([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})";
-      Pattern pattern = Pattern.compile(regex);
-      Matcher matcher = pattern.matcher(pathToJob);
-      String jobUUID = null;
-      if (matcher.find()) {
-         jobUUID = matcher.group(1);
-      } else {
-         // FIXME
-         logger.error("No receivers found for: " + pathToJob);
-         return oldExchange;
-      }
+      MftPath path;
+      try {
+         path = MftPath.fromString(pathToJob);
+         String jobUUID = path.getJobUUIDSegment().toString();
+         if (jobUUID == null ) throw new IllegalArgumentException("jobuuid is null");
 
-      oldExchange.getIn().setHeader("to", receiverFinder.findReceiverForJob(jobUUID));
-      
-      return oldExchange;
+         oldExchange.getIn().setHeader("to", receiverFinder.findReceiverForJob(jobUUID));
+         
+         return oldExchange;
+      } catch (MftPathException | IllegalArgumentException e) {
+         logger.error("Exception while getting uuid: " + e.getMessage(), e);
+         // TODO camel error handling?
+         throw new RuntimeException(e);
+      }
    }
 
 }
