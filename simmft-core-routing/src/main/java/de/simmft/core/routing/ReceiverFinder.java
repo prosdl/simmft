f70fb4657bc4e395fa0de3d90854fc5d5dddf52b
prosdl <rosendahl@prosdl.de>
2014-04-28 17:15:16 +0200

    Cleaned.

--- simmft-core-routing/src/main/java/de/simmft/core/routing/ReceiverFinder.java
+++ simmft-core-routing/src/main/java/de/simmft/core/routing/ReceiverFinder.java
@@ -2,8 +2,6 @@ package de.simmft.core.routing;
 
 import java.util.List;
 
-import de.simmft.common.path.MftPath;
-
 public interface ReceiverFinder {
    public List<String> findReceiverForJob(String jodUUID) throws MftRoutingException;
 }
